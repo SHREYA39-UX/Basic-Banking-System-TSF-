ec6219cead3fec42bbdf00285295e512e007eb45
SHREYA39-UX <76260374+SHREYA39-UX@users.noreply.github.com>
2021-05-06 18:33:52 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,47 @@
+<!-- navbar --> 
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <style type="text/css">
+        body{margin-top: 15px;
+        }
+       
+          .btn {position:relative;
+    display: inline-block;
+    padding: 15px 30px;
+    margin: 10px 0;
+    color: #fff;
+    text-decoration: none;
+    text-transform: uppercase;
+    font-size: 18px;
+    letter-spacing: 2px;
+    border-radius: 8px;
+    background-color: #9dc5c3;
+    background-image: linear-gradient(315deg, #9dc5c3 0%, #5e5c5c 74%);
+                }
+            
+            .df:hover {
+        color: white;
+        font-size: 16px;
+    background-color: #9dc5c3;
+background-image: linear-gradient(315deg, #9dc5c3 0%, #5e5c5c 74%);
+}
+        </style>
+      </head>
+      <body>
+      <center>
+      <nav  class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php"></a>
+      
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="btn df" class="nav-link" href="index.php">Home</a>
+              </li>
+              
+             
+          </div>
+       </nav>
+	   </center>
+     </body>
+     </html>
